e06ba14680cb0dd95109623f4ee90ea592b7e71d
Ryan Gill <gillryan342@outlook.com>
2026-04-15 22:51:00 +0100

    Token refresh, user is logged out after time now

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
 
-// Create an instance of axios with a base URL for the API
-// This allows us to easily make requests to our backend without repeating the base URL
 const api = axios.create({
   baseURL: 'http://localhost:5001/api',
 });
+
+// Response interceptor - auto logout on expired token
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Token expired or invalid - clear session and redirect to login
+      sessionStorage.removeItem('token');
+      sessionStorage.removeItem('user');
+      delete api.defaults.headers.common['Authorization'];
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
